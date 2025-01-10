d899ec2e37c8830bf529095a3f6e831c36728475
humphrey <dang8080@qq.com>
2025-01-10 12:03:51 +0800

    add@ xmake-docs static site

---
+++ source/xmake-docs/assets/chunks/emoji.hynvey8Q.js
@@ -0,0 +1,1 @@
+const s="/xmake-docs/assets/default.RPh6FAOu.png",a="/xmake-docs/assets/ninja.B0GuAWlP.png",t="/xmake-docs/assets/emoji.BZ90tSN2.png";export{s as _,a,t as b};
---
+++ source/xmake-docs/assets/chunks/option_set_category.7RQ_h9C8.js
@@ -0,0 +1,1 @@
+const s="/xmake-docs/assets/option_set_values.C9yIHNip.png",o="/xmake-docs/assets/option_set_category.BI2R1o1G.gif";export{s as _,o as a};
